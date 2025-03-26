b56f33b1711683291acae6baa905db95aa97ca96
yochidros <yoshiki.myzw@gmail.com>
2025-03-26 22:39:54 +0900

    set indent 2.

--- main.swift
+++ main.swift
@@ -1,31 +1,31 @@
 import Foundation
 
 func swiftStrtol(_ str: String, base: Int = 10) -> (value: Int?, rest: Substring) {
-    var index = str.startIndex
-    while index != str.endIndex {
-        let prefix = str[str.startIndex ... index]
-        if Int(prefix, radix: base) == nil {
-            break
-        }
-        index = str.index(after: index)
-    }
+	var index = str.startIndex
+	while index != str.endIndex {
+		let prefix = str[str.startIndex ... index]
+		if Int(prefix, radix: base) == nil {
+			break
+		}
+		index = str.index(after: index)
+	}
 
-    let numberPart = str[str.startIndex ..< index]
-    let rest = str[index...]
-    let value = Int(numberPart, radix: base)
+	let numberPart = str[str.startIndex ..< index]
+	let rest = str[index...]
+	let value = Int(numberPart, radix: base)
 
-    return (value, rest)
+	return (value, rest)
 }
 
 func printInstruction(op: String, args: String...) {
-    print("\t\(op)\t\(args.joined(separator: ", "))")
+	print("\t\(op)\t\(args.joined(separator: ", "))")
 }
 
 let args = CommandLine.arguments
 
 guard args.count == 2 else {
-    print("\(args[0]): invalid number of arguments")
-    exit(1)
+	print("\(args[0]): invalid number of arguments")
+	exit(1)
 }
 
 var str = args[1]
@@ -37,22 +37,22 @@ var rest: Substring?
 let (value, _r) = swiftStrtol(str)
 rest = _r
 if let value {
-    printInstruction(op: "mov", args: "w0", "#\(value)")
+	printInstruction(op: "mov", args: "w0", "#\(value)")
 }
 
 while var r = rest {
-    guard let op = r.popFirst() else { break }
-    let (value, _r) = swiftStrtol(String(r))
-    rest = _r
-    guard let value else { break }
-    switch op {
-    case "+":
-        printInstruction(op: "add", args: "w0", "w0", "#\(value)")
-    case "-":
-        printInstruction(op: "sub", args: "w0", "w0", "#\(value)")
-    default:
-        fatalError("unknown operator: \(op)")
-    }
+	guard let op = r.popFirst() else { break }
+	let (value, _r) = swiftStrtol(String(r))
+	rest = _r
+	guard let value else { break }
+	switch op {
+	case "+":
+		printInstruction(op: "add", args: "w0", "w0", "#\(value)")
+	case "-":
+		printInstruction(op: "sub", args: "w0", "w0", "#\(value)")
+	default:
+		fatalError("unknown operator: \(op)")
+	}
 }
 
 print("\tret")
